c805106c789d795bedabed00099a5e4587c73bf8
LumiLumes <mikehernandez3097@gmail.com>
2025-05-15 00:28:13 -0400

    Fix: added intents to bot

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from discord.ext import commands
 from keep_alive import keep_alive
 import os
+import discord
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'✅ Logged in as {bot.user.name}')
+    print(f"✅ Logged in as {bot.user.name}")
 
 @bot.command()
 async def ping(ctx):
