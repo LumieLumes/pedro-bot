55edbfd89013b937fd46004880a619b239d30175
LumiLumes <mikehernandez3097@gmail.com>
2025-05-15 00:34:16 -0400

    Add keep_alive Flask server

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,13 @@
+# keep_alive.py
+
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Pedro is alive!"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
